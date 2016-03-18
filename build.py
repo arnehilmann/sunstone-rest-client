5b0abd06d5609b275f4ce9c3e9f2b634fd18fe00
Arne Hilmann <Arne.Hilmann@epost-dev.de>
2016-03-18 21:52:22 +0100

    initial version, allows fetch of templates and vms, and can instantiate vms

---
+++ build.py
@@ -0,0 +1,50 @@
+from pybuilder.core import use_plugin, init, Author
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+use_plugin('copy_resources')
+use_plugin('filter_resources')
+
+name = "sunstone-rest-client"
+version = '0.0.1'
+
+default_task = ["clean", "analyze", "publish"]
+
+authors = [Author('Arne Hilmann', 'arne.hilmann@gmail.com')]
+summary = 'sunstone rest client | an opennebula client in python, using the sunstone ReST API'
+description = """sunstone rest client
+
+an opennebula client in python, using the sunstone ReST API
+instead of the XML-RPC
+"""
+url = ''
+license = 'proprietary'
+
+
+@init
+def set_properties(project):
+    project.build_depends_on('mock')
+    project.build_depends_on('testfixtures')
+
+    project.depends_on("requests")
+    project.depends_on("click")
+    project.depends_on("bs4")
+
+    project.set_property('coverage_threshold_warn', 75)
+    project.set_property('coverage_break_build', True)
+
+    project.set_property('flake8_verbose_output', True)
+    project.set_property('flake8_include_test_sources', True)
+    project.set_property('flake8_ignore', 'E501,E402,E731')
+    project.set_property('flake8_break_build', True)
+
+    project.rpm_release = 0
+
+    project.set_property('copy_resources_target', '$dir_dist')
+    project.get_property('copy_resources_glob').append('setup.cfg')
+    project.get_property('filter_resources_glob').append('setup.cfg')
+    project.set_property('distutils_commands', ['sdist'])
