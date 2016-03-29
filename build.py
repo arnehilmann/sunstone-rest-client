9927be615c4fd5ba4c60993552063703931d2cc9
Arne Hilmann <Arne.Hilmann@epost-dev.de>
2016-03-29 16:27:49 +0200

    bump version

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ use_plugin('copy_resources')
 use_plugin('filter_resources')
 
 name = "sunstone-rest-client"
-version = '0.0.7'
+version = '0.0.8'
 
 default_task = ["clean", "analyze", "publish"]
 
