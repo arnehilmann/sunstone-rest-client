9b02ef9afdda6fa7645a9e29fff2acf9b97351ab
Arne Hilmann <Arne.Hilmann@epost-dev.de>
2016-03-29 15:37:07 +0200

    add csrftoken test

--- src/unittest/python/login_tests.py
+++ src/unittest/python/login_tests.py
@@ -14,5 +14,11 @@ class LoginTests(unittest.TestCase):
     def test_login(self):
         sunstone_rest_client.RestClient("http://foobar:4711").login("user", "passwd")
 
+    def test_find_csrf_token(self):
+        html = """<httml><bla></bla><script>tadahh</script><script>var csrftoken='foo';</script></html>"""
+        token = sunstone_rest_client.find_csrftoken(html)
+        self.assertEquals(token, "foo")
+
+
 if __name__ == "__main__":
     unittest.main()
